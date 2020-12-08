a3387c227196bcf3815d864fdec88bfadb307aa4
Semir1998 <75573394+Semir1998@users.noreply.github.com>
2020-12-07 21:43:47 -0500

    Add files via upload

---
+++ preproposal.py
@@ -0,0 +1,23 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+# emulate a pacman
+#create chess or tic tac
+#app that oragnizes roommate's chores
+#app that stores a your meds intake 
+#app that tracks your daily calorie intake
+
+TODO
+
+## If you plan to collaborate with one or two classmates, what are their names?
+#Semir Ibrahim
+
+
+TODO
+
+## Do you have any questions of your own?
+#How and Can you create tables with python?
+#How can you use python to make an app?
+
+TODO
